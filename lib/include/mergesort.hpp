c1f6e644f0b8653a35b996bc8d88709f5487cdf2
FedericoBustaffa <kiko.bustaffa@gmail.com>
2025-07-27 17:10:38 +0200

    omp remove

--- lib/include/mergesort.hpp
+++ lib/include/mergesort.hpp
@@ -8,8 +8,6 @@
 // --- internal.cpp ---
 void mergesort(std::vector<record>& v);
 
-void omp_mergesort(std::vector<record>& v);
-
 // --- external.cpp ---
 void mergesort(const char* filepath, uint64_t limit);
 
